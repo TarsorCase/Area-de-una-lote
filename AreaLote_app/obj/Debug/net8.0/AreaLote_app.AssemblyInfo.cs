a20f19f30a4657b7caee564fb5f7b029fe22a3d1
TarsorCase <gerardgonzalez41@gmail.com>
2024-04-02 20:11:50 -0400

    Cambios necesarios

--- AreaLote_app/obj/Debug/net8.0/AreaLote_app.AssemblyInfo.cs
+++ AreaLote_app/obj/Debug/net8.0/AreaLote_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AreaLote_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736e73361171e160fa3960e048e0322be23cd146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c574e0eafc65dddd0f39afb00b0e016dac0d98a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AreaLote_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AreaLote_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
